75316495ddf64036fab3e0d7367b4997196d9516
Farid <siberia.on@gmail.com>
2020-10-18 18:47:18 +0500

    Добавил задание 4: Пирамида

--- 005_turtle.py
+++ 005_turtle.py
@@ -23,7 +23,7 @@ if x == 1:              # Буква S
     turtle.forward(50)
     turtle.right(90)
     turtle.forward(50)
-    time.sleep(3)  # Задержка 3 секунды
+    time.sleep(3)       # Задержка 3 секунды
 elif x == 2:            # Квадрат
     turtle.shape('turtle')
     turtle.forward(150)
@@ -41,6 +41,27 @@ elif x == 3:            # Окружность
         turtle.left(1)
         turtle.forward(1)
         n -= 1
+    time.sleep(3)
+elif x == 4:            # Пирамида
+    turtle.shape('turtle')
+    n = 15              # длина грани
+    m = 1               # счетчик проходов
+    while m <= 10:
+        turtle.forward(n)
+        turtle.left(90)
+        turtle.forward(n)
+        turtle.left(90)
+        turtle.forward(n)
+        turtle.left(90)
+        turtle.forward(n)
+        n += 30         # увеличиваем длину грани
+        turtle.penup()
+        turtle.forward(15)
+        turtle.right(90)
+        turtle.forward(15)
+        turtle.right(180)
+        turtle.pendown()
+        m += 1          # увеличиваем счетчик на единицу
     time.sleep(3)
 else:
     print('Выбран неверный номер фигуры!')
