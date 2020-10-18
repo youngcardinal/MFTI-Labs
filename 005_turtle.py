d0c2ee6a0c977f9294a454aa26b75b59c71e8408
Farid <siberia.on@gmail.com>
2020-10-18 16:08:23 +0500

    Добавлены подзадачи и встрен начальный цикл, установлена задержка экрана

--- 005_turtle.py
+++ 005_turtle.py
@@ -1,12 +1,36 @@
 import turtle
+import time
 
-turtle.shape('turtle')
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.right(90)
-turtle.forward(50)
-turtle.right(90)
-turtle.forward(50)
+print("Задача:\nНарисовать черепашкой выбранную фигуру\n"
+      "Введите номер выбранной фигуры:\n"
+      "1: Буква S\n"
+      "2: Квадрат\n"
+      "3: Круг\n"
+      "4: Пирамида\n"
+      "5: Звезда\n"
+      "6: Спираль\n"
+      "7: Квадратная спираль\n"
+      "8: Звезда Давида")
+x = int(input())
+if x == 1:
+    turtle.shape('turtle')
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    time.sleep(3)  # Задержка 3 секунды
+else:
+    turtle.shape('turtle')
+    turtle.forward(150)
+    turtle.left(90)
+    turtle.forward(150)
+    turtle.left(90)
+    turtle.forward(150)
+    turtle.left(90)
+    turtle.forward(150)
+    time.sleep(3)
